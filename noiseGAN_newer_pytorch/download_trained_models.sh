5c6a36c795fd68e7d2a8c5c508462aa21efcadb2
Bernardo Henz <bernardohenz@gmail.com>
2020-08-05 14:17:53 -0300

    finishing work non noiseGAN_newerPyTorch

--- noiseGAN_newer_pytorch/download_trained_models.sh
+++ noiseGAN_newer_pytorch/download_trained_models.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd checkpoints/
 
-fileid="1uiPvJvqACe6WFTzY8xRftvmErnR44U1J"
+fileid="1IfsZJG_T8vtEgdJuqEdkYaJ5aRFub51K"
 filename="checkpoints.zip"
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
