a2c3047338e34d3c4ce48fd68b089f962e8cf46b
Eduardo Gastal <eslgastal@inf.ufrgs.br>
2020-08-12 08:16:40 -0300

    Update trained model fileid in download_trained_models.sh script.

--- noiseGAN_newer_pytorch/download_trained_models.sh
+++ noiseGAN_newer_pytorch/download_trained_models.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd checkpoints/
 
-fileid="1D8y9WrBzwtENqG4U_YkDI2Go4lfpGPpj"
+fileid="1x_ONMjQEGxKU-F1zfbv1qluhYyOLa0z4"
 filename="checkpoints.zip"
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
